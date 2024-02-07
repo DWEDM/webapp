3e1bcfadbd45a0bc21dec8a3a03b1cb7ad6c50b6
Wane <denver.delamasa@gmail.com>
2024-02-07 09:49:51 +0800

    update

--- mvc/app/core/init.php
+++ mvc/app/core/init.php
@@ -3,6 +3,9 @@
 require 'config.php';
 require 'functions.php';
 require 'Database.php';
+
+## dapat magrrun muna yung database bago yung model, kase need ni model yung database
+
 require 'Model.php';
 require 'Controller.php';
 require 'App.php';
