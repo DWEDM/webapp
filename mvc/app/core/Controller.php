b4201e0d91a7659b839268cb8b19fb11220a8cea
Wane <denver.delamasa@gmail.com>
2024-02-28 08:15:39 +0800

    schoo

---
+++ mvc/app/core/Controller.php
@@ -0,0 +1,15 @@
+<?php
+
+class Controller
+{
+    public funtion view($name)
+    {
+        if(file_exist('../app/views/' . $name . '.php'))
+        {
+            require '../app/views/'. $name .'.php';
+        }
+        else{
+            require '../app/views/404.php';
+        }
+    }
+}
