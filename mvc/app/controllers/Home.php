095e4a317b44eb320a0f7c895b2ae2350522fa31
Wane <denver.delamasa@gmail.com>
2024-03-06 08:47:16 +0800

    bago sa schoo

--- mvc/app/controllers/Home.php
+++ mvc/app/controllers/Home.php
@@ -5,6 +5,11 @@ class Home extends Controller
     // controller -> method ->> ahfajklsdf nicesuuu cyka blyaddd
     public function index()
     {
+        $model = new Model();
+
+        $data = $model->findAll();
+        show($data);
+
         $this->view('home');
     }
 }
