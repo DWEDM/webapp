9e13a4db7cdccd7d5f3c0fa8f5a0707a21174eb1
DENVER SA ISANG LAPTOP <wane.danvers@gmail.com>
2024-03-18 23:15:28 +0800

    UWU UwU OwO OvU

--- main/app/site.php
+++ main/app/site.php
@@ -10,7 +10,7 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     
     
-    
+        <link rel="stylesheet" href="style.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     
@@ -25,9 +25,15 @@
     </head>
 <body>
   <?php include('include/header.php')?>
+
+
+
     <div class="main-container">
         
     </div>
+
+
+
     <?php include('include/footer.php')?>
 </body>
 </html>
