d58261e524fc380033eb1db2c667b05014e44ece
DENVER SA ISANG LAPTOP <wane.danvers@gmail.com>
2024-03-19 02:36:07 +0800

    UWU UwU OwO OvU

--- main/app/include/header.php
+++ main/app/include/header.php
@@ -9,7 +9,9 @@
 <body>
         <!-- navbar -->
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div></div>
+          <div class="logo-container">
+            <img src="../resources/icon.png" alt="site logo" class="logo">
+          </div>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
--- main/app/site.php
+++ main/app/site.php
@@ -5,9 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Homepage</title>
         
-        
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">  
         <link rel="stylesheet" href="style.css">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
@@ -20,23 +20,46 @@
 <body>
   <?php include('include/header.php')?>
 
+  <div class="title-section">
     <div class="title">
       Homepage
     </div>
-    <div class="content">
+    <img class="banner-animated"  src="../resources/banner-animated.gif">
+  </div>
 
+
+
+    <div class="content">
       <div class="main-content">
         <div class="AOTD">
+          <h2>Art of The Day:</h2>
           <div class="image-AOTD">
             <img src="../resources/artworks/Dinner in the sun(1938) - fernando amorsolo.jpg" alt="people eating filipino local foods outside the hut">
+            <h4>Dinner in The Sun - 1938</h4>
+            <h5>By: Fernando Amorsolo</h5>
           </div>
-          <p></p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+            pariatur. Excepteur sint occaecat cupidatat non proident, 
+            sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
         <div class="featured">
+          <h2>Featured Art:</h2>
           <div class="image-featured">
             <img src="../resources/artworks/philippine bululs.jpg" alt="sitting carved human called bululs">
+            <h3>Philippine Bululs</h3>
           </div>
-          <p></p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+            pariatur. Excepteur sint occaecat cupidatat non proident, 
+            sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
         </div>
       </div>
       <div class="announcements">
