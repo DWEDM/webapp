4baee6c54921799694bdeab79d3fba5a1f0ab26c
DENVER SA ISANG LAPTOP <wane.danvers@gmail.com>
2024-05-10 20:29:17 +0800

    mmove ko muna to mvc

---
+++ mvc-test/app/core/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('APP_NAME', 'MVC TEST');
+define('ROOT', 'http://localhost/webapp/mvc-test/public');
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'mvc_db');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
